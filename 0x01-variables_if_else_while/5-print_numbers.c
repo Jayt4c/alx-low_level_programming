02bf15b63adff9c436057f425223feaf749a240d
Jayt4c <igejuwon53@gmail.com>
2023-10-06 07:41:15 -0700

    betty solved for task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+/**
+ * main - print single digit numbers from 0
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-	    char digit;
+	char digit;
 
-	        for (digit = '0'; digit <= '9'; digit++)
-			    {
-				            putchar(digit);
-					        }
-
-		    putchar('\n');
-
-		        return 0;
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
 }
 
