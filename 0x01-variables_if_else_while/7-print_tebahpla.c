75187077f3256649d88307cec738963e0d44dbbc
Jayt4c <igejuwon53@gmail.com>
2023-10-06 08:07:21 -0700

    task 7 solved

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i >= 0; i--)
+	{
+		putchar(alp[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
