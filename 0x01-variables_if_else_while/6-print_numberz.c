53a6aaee4c9c3f7d1c411a90135d81ac968f088f
Jayt4c <igejuwon53@gmail.com>
2023-10-06 08:00:29 -0700

    betty and all solved

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
+i
