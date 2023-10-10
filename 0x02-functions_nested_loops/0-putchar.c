ce7774a0bd5c8c1eea7f1a63cc9a389d4fc14088
Jayt4c <igejuwon53@gmail.com>
2023-10-10 07:19:11 -0700

    putchar can only input single character so i input them separately

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	putchar("_putchar");
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
 
 	return (0);
 }
