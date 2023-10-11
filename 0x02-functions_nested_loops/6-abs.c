5ca64ccd9fee9e9f788fb6c7486dcfc71b2cf2a3
Jayt4c <igejuwon53@gmail.com>
2023-10-11 01:58:09 -0700

    task 5 started

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - computes value of an integer
+ * @n: n is an int
+ *
+ * Return: integer
+ */
+
+int _abs(int n)
+{
+  if (n > 0)
+    {
+      return (n);
+    }
+  else if (n < 0)
+    {
+      return (-n);
+    }
+  else
+    {
+      return (n);
+    }
+}
