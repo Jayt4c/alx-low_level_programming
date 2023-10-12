0f2cde526203347ccd16f10f6f981a6e67ff8893
Jayt4c <igejuwon53@gmail.com>
2023-10-12 07:55:46 -0700

    more functions task done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check for upper
+ *
+ * @c: input char
+ *
+ * Return: 1 if uppercase, 0 if otherwise
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: input int
+ * Return: 1 if is digit, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
