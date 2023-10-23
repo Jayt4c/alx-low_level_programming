cc4963af8a65ec8c19bc7c349dd64c6ab53dcad1
Jayt4c <igejuwon53@gmail.com>
2023-10-23 11:51:32 -0700

    task strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * _strspn - calculate the length of string with accepted characters
- * @string: analyzed string
- * @accepted: accepted characters
- *
- * Return:pointer to destination memory
- */
-unsigned int _strspn(char *string, char *accepted)
+ *   * _strchr - locate character in string
+ *     * @s: source string
+ *       * @c: character to find
+ *         *
+ *           * Return: the string from character found
+ *             */
+char *_strchr(char *s, char c)
 {
-	unsigned int string_index, accepted_index, found;
+		int a = 0, b;
 
-	for (string_index = 0; string[string_index] != '\0'; string_index++)
-	{
-		found = 0;
-		for (accepted_index = 0; accepted[accepted_index] != '\0'; accepted_index++)
-		{
-			if (string[string_index] == accepted[accepted_index])
-			{
-				found = 1;
-				break;
-			}
-		}
-		if (found == 0)
-			break;
-	}
-	return (string_index);
+			while (s[a])
+					{
+								a++;
+									}
+
+				for (b = 0; b <= a; b++)
+						{
+									if (c == s[b])
+												{
+																s += b;
+																			return (s);
+																					}
+										}
+
+					return ('\0');
 }
