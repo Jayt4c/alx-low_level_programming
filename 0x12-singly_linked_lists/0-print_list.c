2f0fcfd9da9cce3a14f6bfb38d1b7c67e9b2a431
Jayt4c <igejuwon53@gmail.com>
2023-11-17 08:08:14 -0800

    betty sorted in task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str != NULL)
-            printf("[%d] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 
-        h = h->next;
-        node_count++;
-    }
+		h = h->next;
+		node_count++;
+	}
 
-    return node_count;
+	return (node_count);
 }
 
