b11dda153971bfe7ff8c67bdf4b58f01f2d5d342
Jayt4c <igejuwon53@gmail.com>
2023-11-17 08:01:29 -0800

    changed to while loop for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,26 @@
-#include "lists.h"
+#include "list.h"
+#include <stdio.h>
 
 /**
- * print_list - print list of elements
- * @h: input
- * Return: Number of Nodes
+ * print_list - print elements of a linked list
+ * @h: pointer to the head of the list
+ * Return: number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-	if (h)
-	{
-		if (h->str)
-			printf("[%d] %s%s", h->len, h->str, "\n");
-		else
-			printf("[%d] %s%s", 0, "(nil)", "\n");
-		if (h->next)
-		{
-			return (1 + print_list(h->next));
-		}
-		return (1);
-	}
-	return (0);
+    size_t node_count = 0;
+
+    while (h != NULL)
+    {
+        if (h->str != NULL)
+            printf("[%d] %s\n", h->len, h->str);
+        else
+            printf("[0] (nil)\n");
+
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
 }
+
