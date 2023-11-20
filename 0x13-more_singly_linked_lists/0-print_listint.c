89de08d1a751ec86fd88ca36bae7f41c1efb081d
Jayt4c <igejuwon53@gmail.com>
2023-11-20 00:47:13 -0800

    more single linked list tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/*
+* print_listint - prints all the elements in the list 
+* @ptr: pointer to the head
+*
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	const listint_t  *ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr -> n);
+		ptr = ptr -> next;
+		count ++;
+	}
+
+	return count;
+}
