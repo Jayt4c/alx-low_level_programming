5c33743f601a2e2ed5e2aa8e97b1d9884a182116
Jayt4c <igejuwon53@gmail.com>
2023-11-20 04:14:31 -0800

    tasks 8 edited 9 and 10 completed

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes the node at a given index of a list.
+ * @head: Pointer to the head of the list.
+ * @index: Index of the node that should be deleted. Index starts at 0.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *temp;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+
+	for (i = 0; i < index - 1 && current != NULL; i++)
+		current = current->next;
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	temp = current->next->next;
+	free(current->next);
+	current->next = temp;
+
+	return (1);
+}
+
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Return sum of all data (n) of a listint_t linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: Sum of all the data in the list, or 0 if the list is empty.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
