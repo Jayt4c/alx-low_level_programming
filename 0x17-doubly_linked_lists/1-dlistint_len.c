171a1de3d4c9d09bff9df203b5d417ec67973615
Jayt4c <igejuwon53@gmail.com>
2024-01-11 05:16:29 -0800

    task 1 and updated lists.h

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all elements of a dlistint_t
+ * dlistint - returns number of elements in a dlistint_t
  * @h: pointer to the head
  *
- * Return: On Success, number of nodes
+ * Return: On Success, number of elements.
  *
  **/
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *current = h;
